9fbd97c1c7d05eff8415357f88febb542bf9e098
Fredwin Rosales <frosales@novopayment.com>
2018-01-04 22:33:29 -0400

    Instalar morgan
    Esto para identificar las peticiones recibidas

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const http = require('http');
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 const server = http.createServer(app);
 
+//Configuración del servidor
 app.set('port', 3000);
+app.use(morgan('dev'));
 
 app.use(express.static(__dirname + '/public'));
 
+//Inicialización del servidor
 server.listen(app.get('port'), function() {
 	console.log('Server started in port 3000');
 });
 
+//Lógica del socket
 require('./sockets')(server);
